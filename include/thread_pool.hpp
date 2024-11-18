8c4e9a4d0be422f973dcd1fb659c91831ea6b832
Matveev Stepan Andreevich <mset321@gmail.com>
2024-11-18 16:57:34 +0300

    refactor: add doxygen dox for all hpp files

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file thread_pool.hpp
+ * @author Matveev S.A. (mset321@gmail.com)
+ * @brief Header file defining a thread pool class for concurrent task execution.
+ * @version 0.1
+ * @date 2024-11-18
+ * 
+ * @copyright Copyright (c) 2024 Matveev S.A.
+ * Licensed under the MIT License. See LICENSE file for details.
+ */
+
 #ifndef INCLUDE_THREAD_POOL_HPP
 #define INCLUDE_THREAD_POOL_HPP
 
@@ -32,16 +43,25 @@ void consumerWork(thread_queue::UnboundedTaskQueue& queue, std::latch& L) {
 }  // namespace detail__
 
 namespace task_id {
-    using taskID_t = size_t;
-    taskID_t initID() noexcept;
-    taskID_t getNextId(const taskID_t& ID) noexcept;
-}
+using taskID_t = size_t;
+taskID_t initID() noexcept;
+taskID_t getNextId(const taskID_t& ID) noexcept;
+}  // namespace task_id
 
 namespace impl__ {
 template <typename T>
 class ThreadPool_impl {
 
    public:
+    /**
+     * @brief Pushes a task into the thread pool and returns a task ID.
+     * 
+     * @tparam F Callable type for the task function.
+     * @tparam Args Types of the arguments to be passed to the task function.
+     * @param f The task function.
+     * @param args Arguments to be passed to the task function.
+     * @return task_id::taskID_t Unique identifier for the submitted task.
+     */
     template <typename F, typename... Args>
     task_id::taskID_t pushTask(F f, Args&&... args) {
         auto [tsk, fut] =
@@ -74,7 +94,7 @@ class ThreadPool_impl {
    private:
     std::vector<std::thread> consumers_;
     thread_queue::UnboundedTaskQueue queue_;
-    task_id::taskID_t curtask_id::taskID_t_ = initID();
+    task_id::taskID_t curtask_id = task_id::initID();
     std::latch threadsWaiter_;
 
    private:
@@ -84,7 +104,7 @@ class ThreadPool_impl {
     }
 
     task_id::taskID_t moveID_() {
-        return std::exchange(curtask_id::taskID_t_, getNextId(curtask_id::taskID_t_));
+        return std::exchange(curtask_id, task_id::getNextId(curtask_id));
     }
 
     void setConsumers_(size_t count) {
@@ -107,27 +127,66 @@ class ThreadPool_impl {
 
 namespace thread_pool {
 
+/**
+ * @brief A class providing an interface for executing tasks in a multithreaded environment.
+ * 
+ * @tparam T The value returned by the task.
+ */
 template <class T>
 class ThreadPool final : public impl__::ThreadPool_impl<T> {
    public:
+    /**
+     * @brief Default constructor for the ThreadPool class specialized for void tasks.
+     */
     ThreadPool() = default;
 
+    /**
+     * @brief Constructs a thread pool for void tasks with a specified number of threads.
+     * 
+     * @param threadCount Number of threads to initialize in the pool.
+     */
     ThreadPool(size_t threadCount) : impl__::ThreadPool_impl<T>(threadCount) {}
 
+    /**
+     * @brief Waits for a specific task to complete and retrieves its result.
+     * 
+     * @tparam T The type of the result produced by the task.
+     * @param ID The unique identifier of the task.
+     * @param res Reference to store the result of the completed task.
+     */
     void waitNPopResult(const task_id::taskID_t& ID, T& res) {
         res = impl__::ThreadPool_impl<T>::future_[ID].get();
         impl__::ThreadPool_impl<T>::future_.erase(ID);
     }
 };
 
+/**
+ * @brief Specialization of ThreadPool for void.
+ * Implies that the task does not return a value.
+ * 
+ * @tparam void
+ */
 template <>
 class ThreadPool<void> final : public impl__::ThreadPool_impl<void> {
    public:
+    /**
+     * @brief Default constructor for the ThreadPool class specialized for void tasks.
+     */
     ThreadPool() = default;
 
+    /**
+     * @brief Constructs a thread pool for void tasks with a specified number of threads.
+     * 
+     * @param threadCount Number of threads to initialize in the pool.
+     */
     ThreadPool(size_t threadCount)
         : impl__::ThreadPool_impl<void>(threadCount) {}
 
+    /**
+     * @brief Waits for a specific void task to complete.
+     * 
+     * @param ID The unique identifier of the task.
+     */
     void waitTask(const task_id::taskID_t& ID) {
         impl__::ThreadPool_impl<void>::future_[ID].get();
         impl__::ThreadPool_impl<void>::future_.erase(ID);
--- include/thread_queue.hpp
+++ include/thread_queue.hpp
@@ -1,3 +1,20 @@
+/**
+ * @file queue_pool.hpp
+ * @author Matveev S.A. (mset321@gmail.com)
+ * 
+ * @brief Provides an interface for creating and managing tasks in a multithreaded queue system.
+ * 
+ * @version 0.1
+ * @date 2024-11-18
+ * 
+ * @copyright Copyright (c) 2024 Matveev S.A.
+ * Licensed under the MIT License. See LICENSE file for details.
+ * 
+ * This file contains the declaration of the `UnboundedTaskQueue` class, which represents an unbounded queue
+ * for handling tasks in a thread-safe manner. Additionally, it provides the `createTask` function template
+ * for generating tasks that can be pushed into the queue.
+ */
+
 #ifndef INCLUDE_QUEUE_POOL_HPP
 #define INCLUDE_QUEUE_POOL_HPP
 
@@ -10,13 +27,37 @@
 
 namespace thread_queue {
 
+/**
+ * @typedef task_t
+ * @brief Represents a move-only function type that returns an `int`.
+ */
 using task_t = std::move_only_function<int()>;
 
-// lock based queue
+/**
+ * @class UnboundedTaskQueue
+ * @brief A thread-safe unbounded queue for managing tasks.
+ *
+ * The `UnboundedTaskQueue` class provides a lock-based mechanism for
+ * pushing and popping tasks in a multithreaded environment. It ensures
+ * thread safety by using a mutex and condition variable.
+ */
 class UnboundedTaskQueue {
    public:
+    /**
+     * @brief Pushes a new task into the queue.
+     * @param tsk The task to be added to the queue.
+     */
     void pushTask(task_t&& tsk);
+
+    /**
+     * @brief Pops a task from the queue.
+     * @param tsk Reference to the task that will be set when popped.
+     */
     void popTask(task_t& tsk);
+
+    /**
+     * @brief Marks the queue as done, indicating no more tasks will be added.
+     */
     void setDone();
 
    private:
@@ -30,6 +71,21 @@ class UnboundedTaskQueue {
     bool isDone_() const noexcept;
 };
 
+/**
+ * @brief Creates a task that can be executed in a multithreaded environment.
+ *
+ * This function template takes a C++ function or callable object `F` as an argument
+ * and binds any additional arguments passed to it. It returns a pair consisting of
+ * a task ready to be executed and a future to obtain the result.
+ *
+ * @tparam F The type of the function to be invoked. Must be a C++ function.
+ * @tparam Args The types of additional arguments to be passed to the function.
+ * @param f A callable object (C++ function) that represents the task.
+ * @param args Additional arguments to be passed to the function when it is executed.
+ * @return A `std::pair` containing the created task (`task_t`) and a `std::future` for the result.
+ *
+ * @pre The template parameter `F` must be a valid C++ function.
+ */
 template <typename F, typename... Args>
 auto createTask(F f, Args&&... args) {
     std::packaged_task<std::remove_pointer_t<F>> packagedF{f};
